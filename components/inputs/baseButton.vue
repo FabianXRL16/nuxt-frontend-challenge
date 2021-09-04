c8d38666170a1ce8fd7bac9883ee6a6aa2b75487
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:16:36 -0500

    Generic button

---
+++ components/inputs/baseButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="bg-blue-500 rounded-xl text-white text-xl">
+    Add of Character
+  </button>
+</template>
+
+<script>
+export default {
+  name: "baseButton"
+};
+</script>
