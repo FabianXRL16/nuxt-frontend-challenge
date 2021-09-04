504d88c7f3fa261acf0f051a74167d1809112e54
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:11:31 -0500

    Layout default created

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-      Layout
+    <div
+      class="w-screen h-screen flex flex-col items-center 
+      py-16 md:bg-blue-100 bg-blue-200 bg-opacity-50 
+      md:bg-opacity-none"
+    >
       <Nuxt />
+    </div>
+    <shadowForm v-if="showForm" />
   </div>
 </template>
+
+<script>
+import shadowForm from "@/components/general/shadowForm";
+export default {
+  components: {
+    shadowForm
+  },
+  computed: {
+    showForm() {
+      return this.$store.getters["getUp"];
+    }
+  }
+};
+</script>
