54b67f0eba1dc7e11fa124129741ace15c0b0dff
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:15:35 -0500

    Component avatar

---
+++ components/general/avatar.vue
@@ -0,0 +1,9 @@
+<template>
+    <img class="h-12 w-12 rounded-full" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="">
+</template>
+
+<script>
+export default{
+    name: "avatar"
+}
+</script>
