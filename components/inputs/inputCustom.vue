4d1f465093b15849faf6a3e2be4a5cc2acc90727
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:18:09 -0500

    Generic input text

---
+++ components/inputs/inputCustom.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col gap-2 rounded-xl px-4">
+    <label class="text-gray-600">{{ label }}</label>
+    <input
+      class="outline-none bg-gray-200 px-4 text-gray-500 rounded-xl h-12"
+      type="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputCustom",
+  props: {
+    label: {
+      type: String,
+      default: "Label"
+    }
+  }
+};
+</script>
