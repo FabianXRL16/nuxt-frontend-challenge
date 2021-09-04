f94a59d4bd2dbf7d2a8cb0be3e443351d0ad03c2
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:20:22 -0500

    Component see more details of character

---
+++ components/items/detailsCharacter.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    href="#"
+    class="text-blue-600 hover:text-blue-900
+    text-sm font-medium"
+  >
+    see more
+  </a>
+</template>
+
+<script>
+export default {
+  name: "detailsCharacter"
+};
+</script>
