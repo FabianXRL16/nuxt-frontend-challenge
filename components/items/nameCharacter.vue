27a3298f25fdb037adba00a4bff81fe116e91ecc
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:19:59 -0500

    Component of name character

---
+++ components/items/nameCharacter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-center border-box">
+    <div class="flex-shrink-0 h-12 w-12">
+      <avatar />
+    </div>
+    <div class="ml-4">
+      <div class="font-medium text-gray-500 md:pb-1">
+        Abadango Cluster Princess
+      </div>
+      <div class="text-sm text-gray-500 flex gap-2 divide-x divide-gray-400">
+        <div>
+          <span>Humano</span>
+        </div>
+        <div class="pl-2 hidden md:table-cell">
+          <span>Male</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import avatar from "@/components/general/avatar";
+export default {
+  name: "nameCharacter",
+  components: {
+    avatar
+  }
+};
+</script>
