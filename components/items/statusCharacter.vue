aef135aa06f61b7798ddc88c96ba4b6a8690f8fc
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:21:23 -0500

    Status of character

---
+++ components/items/statusCharacter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-20 rounded-full bg-green-400 grid place-content-center">
+    <span class="text-xs leading-5 font-semibold text-white">
+      Alive
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "statusCharacter"
+};
+</script>
