30af701d94ddd5c6a9e047541416295542791386
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:12:11 -0500

    Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-    Mostarr
+  <div
+    class="w-full md:pt-0 md:pb-8 max-w-screen-md box-border
+      mx-auto md:bg-white md:bg-opacity-50 md:rounded-xl fixed relative"
+  >
+    <mainContainer />
   </div>
 </template>
 
 <script>
-export default {}
+import mainContainer from "@/components/containers/mainContainer";
+import shadowForm from "@/components/general/shadowForm";
+export default {
+  components: {
+    mainContainer,
+    shadowForm
+  },
+  data() {
+    return {};
+  },
+};
 </script>
