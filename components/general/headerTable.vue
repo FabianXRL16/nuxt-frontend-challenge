2cac03f5a86a0467d8d8e248d0243b7ab0f60f28
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:17:49 -0500

    Character table header

---
+++ components/general/headerTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr>
+    <th
+      v-for="(header, index) in headersList"
+      :key="index"
+      class="hName"
+      :class="header.style"
+    >
+      {{ header.title }}
+    </th>
+  </tr>
+</template>
+
+<script>
+import headersList from "@/global/headerList";
+export default {
+  name: "headerList",
+  data() {
+    return {
+      headersList
+    };
+  }
+};
+</script>
+
+<style scoped>
+.hName:nth-child(1) {
+  min-width: 230px;
+}
+</style>
