cb7a431d4ed8bc43c660393c01de149d96e9acad
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:21:41 -0500

    Modal form shadow

---
+++ components/general/shadowForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form w-screen h-screen flex justify-center items-center">
+    <form-character />
+  </div>
+</template>
+
+<script>
+import FormCharacter from "@/components/containers/formCharacter";
+export default {
+  name: "ShadowForm",
+  components: {
+    FormCharacter
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  position: absolute;
+  top: 0rem;
+  background-color: rgba(0, 0, 0, .5);
+}
+</style>
