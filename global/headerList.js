38ffb2fea8c2ea444cf8737be102bc753c9fb179
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-04 16:12:59 -0500

    Headers of the table

---
+++ global/headerList.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "character",
+    style: "px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+  },
+  {
+    title: "status",
+    style: "w-28 px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:table-cell"
+  },
+  {
+    title: "created",
+    style: "w-28 pl-6 py-3 box-border text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:table-cell"
+  },
+  {
+    title: "details",
+    style: "w-28 pl-8 py-3 box-border text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:table-cell"
+  },
+];
