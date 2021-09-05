244c0dc32bbd7d8a90e389c4c71e62e4ef0a6157
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 01:03:42 -0500

    Async await in loadCharacters

--- store/index.js
+++ store/index.js
@@ -11,28 +11,36 @@ export const getters = {
   },
   getCharacters(state) {
     return state._characters;
-  },
+  }
 };
 
 export const actions = {
   showForm({ commit }) {
     commit("changeUp");
   },
-  loadCharacters(state) {
-    axios
-      .get("https://rickandmortyapi.com/api/character")
-      .then(res => {
-        state.commit("SET_CHATRACTERS",res.data.results)
-      })
-      .catch(e => console.error(e));
-  }
+  async loadCharacters({commit}) {
+    try {
+      const api = "https://rickandmortyapi.com/api/character";
+      const data = await (await axios.get(api)).data.results;
+      commit("SET_CHATRACTERS", data);
+    } catch (error) {
+      const err = new Error(error);
+      console.error(err);
+    }
+    // axios
+    //   .get("https://rickandmortyapi.com/api/character")
+    //   .then(res => {
+    //     state.commit("SET_CHATRACTERS",res.data.results)
+    //   })
+    //   .catch(e => console.error(e));
+  },
 };
 
 export const mutations = {
   changeUp(state) {
     state.up = !state.up;
   },
-  SET_CHATRACTERS(state, characters){
+  SET_CHATRACTERS(state, characters) {
     state._characters = characters;
   }
 };
