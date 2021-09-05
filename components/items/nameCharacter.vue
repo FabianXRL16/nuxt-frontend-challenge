26c54824c1e68ab7e2da41be9421d9b338ce667e
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 13:32:00 -0500

    Change of sending data

--- components/items/nameCharacter.vue
+++ components/items/nameCharacter.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex items-center border-box">
     <div class="flex-shrink-0 h-12 w-12">
-      <avatar :image="data.image" />
+      <avatar :image="image" :name="name" />
     </div>
     <div class="ml-4">
       <div class="font-medium text-gray-500 md:pb-1">
-        {{ data.name }}
+        {{ name }}
       </div>
       <div class="text-sm text-gray-500 flex gap-2 divide-x divide-gray-400">
         <div>
-          <span>{{ data.species }}</span>
+          <span>{{ species }}</span>
         </div>
         <div class="pl-2 hidden md:table-cell">
-          <span> {{ data.gender }} </span>
+          <span> {{ gender }} </span>
         </div>
       </div>
     </div>
@@ -27,9 +27,20 @@ export default {
     avatar
   },
   props:{
-   data:{
-     type: Object,
-     default: () => {}
+   name:{
+     type: String,
+     default: "Not Name"
+   },gender:{
+     type: String,
+     default: "Unknowk"
+   },
+   species:{
+     type: String,
+     default: "Lost"
+   },
+   image:{
+     type: String,
+     default: ""
    }
   }
 };
