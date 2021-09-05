2b9431178a6bc52d96de9c35f22e629f3ca5d2de
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 00:37:29 -0500

    Component character dinamic

--- components/items/characters.vue
+++ components/items/characters.vue
@@ -1,19 +1,19 @@
 <template>
   <tr class="hover:bg-blue-200 md:hover:bg-blue-100">
-    <td class="md:px-6 py-4 whitespace-nowrap rounded-l-xl">
-      <name-character />
+    <td class="md:px-6 px-4 py-4 whitespace-nowrap rounded-l-xl">
+      <name-character :name="character.name" :species="character.species" :gender="character.gender" :image="character.image" />
     </td>
     <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell w-20">
-      <status-character />
+      <status-character :status="character.status" />
     </td>
     <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell w-20">
-      <createdCharacter />
+      <createdCharacter :created="character.created" />
     </td>
     <td
-      class="md:px-6  px-0 py-6 whitespace-nowrap md:text-left
+      class="md:px-6 px-4 py-6 whitespace-nowrap md:text-left
       text-right rounded-r-xl w-20"
     >
-      <detailsCharacter />
+      <detailsCharacter :details="character.url" />
     </td>
   </tr>
 </template>
@@ -30,6 +30,12 @@ export default {
     statusCharacter,
     detailsCharacter,
     createdCharacter
+  },
+  props:{
+    character:{
+      type: Object,
+      default: () => {}
+    }
   },
   data() {
     return {};
