543066f23525c8bc54471adaf83f90fcba64eede
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 00:37:06 -0500

    Component avatar dinamic

--- components/general/avatar.vue
+++ components/general/avatar.vue
@@ -1,9 +1,15 @@
 <template>
-    <img class="h-12 w-12 rounded-full" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="">
+    <img class="h-12 w-12 rounded-full" :src="image" alt="">
 </template>
 
 <script>
 export default{
-    name: "avatar"
+    name: "avatar",
+    props:{
+        image:{
+            type: String,
+            default: ""
+        }
+    }
 }
 </script>
