c3a338fd5b58d2d4df2cbf7d65e23da5448b1035
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 00:38:31 -0500

    Component nameCharacter dinamic

--- components/items/nameCharacter.vue
+++ components/items/nameCharacter.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex items-center border-box">
     <div class="flex-shrink-0 h-12 w-12">
-      <avatar />
+      <avatar :image="image" />
     </div>
     <div class="ml-4">
       <div class="font-medium text-gray-500 md:pb-1">
-        Abadango Cluster Princess
+        {{ name }}
       </div>
       <div class="text-sm text-gray-500 flex gap-2 divide-x divide-gray-400">
         <div>
-          <span>Humano</span>
+          <span>{{ species }}</span>
         </div>
         <div class="pl-2 hidden md:table-cell">
-          <span>Male</span>
+          <span> {{ gender }} </span>
         </div>
       </div>
     </div>
@@ -25,6 +25,24 @@ export default {
   name: "nameCharacter",
   components: {
     avatar
+  },
+  props:{
+    name:{
+      type: String,
+      default: "Rick Sanchez"
+    },
+    species:{
+      type: String,
+      default: "Human"
+    },
+    gender:{
+      type: String,
+      default: "Male"
+    },
+    image:{
+      type: String,
+      default: ""
+    }
   }
 };
 </script>
