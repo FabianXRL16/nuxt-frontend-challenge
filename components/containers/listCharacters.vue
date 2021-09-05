a4ed869d2fd4e72fd2fcd168705cae38abb3eef0
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-05 00:37:17 -0500

    Component list dinamic

--- components/containers/listCharacters.vue
+++ components/containers/listCharacters.vue
@@ -6,7 +6,7 @@
     <div class="container">
       <table class="w-full divide-y divide-gray-300">
         <tbody class="divide-y md:divide-gray-200 divide-gray-300">
-          <characters v-for="n in 20" :key="n" />
+          <characters v-for="character in getData" :key="character.id" :character="character" />
         </tbody>
       </table>
     </div>
@@ -16,12 +16,42 @@
 <script>
 import headerTable from "@/components/general/headerTable";
 import characters from "@/components/items/characters";
+// import data from "../../src/api/characters.js";
+// import axios from "axios";
 export default {
   name: "ListCharacters",
   components: { headerTable, characters },
   data() {
-    return {};
+    return {
+      data: []
+    };
+  },
+  
+  computed:{
+    characters(){
+      return data
+    },
+    getData(){
+      return this.$store.getters['getCharacters']
+    }
+  },
+  created(){
+    this.$store.dispatch("loadCharacters")
   }
+
+  // methods:{
+  //   getDataApi(){
+  //     axios.get('https://rickandmortyapi.com/api/character')
+  //     .then(response=>{
+  //       this.data = response.data.results
+  //       console.log(response.data.results)
+  //     })
+  //     .catch(e => {console.log(e)})
+  //   }
+  // },
+  // mounted(){
+  //   this.getDataApi()
+  // }
 };
 </script>
 
